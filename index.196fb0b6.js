3353652659cd55757fe7edae2a61e1b5e6be6984
Marcin Maka <marcinmaka37@gmail.com>
2025-01-11 00:16:06 +0100

    make build

---
+++ index.196fb0b6.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector("#a-button").addEventListener("click",function(e){e.preventDefault()},!1);
+//# sourceMappingURL=index.196fb0b6.js.map
--- index.d442c3ea.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
-//# sourceMappingURL=index.d442c3ea.js.map
